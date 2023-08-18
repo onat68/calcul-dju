1019bdba38668cb9932ec057c8c29fd7bdd51258
onat68 <onat.rigault@gmail.com>
2023-08-18 15:52:05 +0200

    calcul de la moyenne DJU pour chaque hiver

--- DJURouen.js
+++ DJURouen.js
@@ -5,7 +5,7 @@ let dataStation = []
 
 fs.readFile("./dataStations/dataRouen.csv", "utf-8", (err, data) => {
   if (err) console.log(err);
-  else console.log("all good");
+  else console.log("Démarré");
 });
 
 fs.createReadStream("./dataStations/dataRouen.csv")
@@ -26,6 +26,7 @@ const extractTemperatures = (result) => {
 
         if (!sortedTemperature[date] && element.temperature && date.split('-')[0]!='2023') {
             sortedTemperature[date] = {
+              date: date,
               tmin: parseFloat(element.temperature),
               tmax: parseFloat(element.temperature),
               DJU: calculDJU(parseFloat(element.temperature),parseFloat(element.temperature))
@@ -40,21 +41,59 @@ const extractTemperatures = (result) => {
             }
         }
     });
-    
-    let totalDJU = 0
-    let totalDays = 0
-    for(date in sortedTemperature){
-        totalDays += 1
-        // console.log(sortedTemperature[date],date)
-        totalDJU += sortedTemperature[date].DJU
-    }
 
-    let moyenneDJU = totalDJU / totalDays
-
-    console.log(sortedTemperature)
-
-    
+    console.log('Moyenne décennie : ' , calculDJUDecennie(2011,2012))
 }
+
+const extractWinterOf = (startYear,endYear) => {
+  let arrayOfWinter = new Array()
+
+  let startDate = new Date(`${startYear}-10-01`)
+  let endDate = new Date(`${endYear}-05-31`)
+
+
+  for (date in sortedTemperature){
+    let dateToCompare = new Date(date)
+    if (startDate <= dateToCompare && dateToCompare <= endDate){
+      arrayOfWinter.push(sortedTemperature[date])
+      // console.log(arrayOfWinter)
+    }
+  }
+  return arrayOfWinter
+
+}
+
+const calculDJUMoyen = (arrayOfDates) => {
+  let totalDJU = 0
+  let totalDays = 0
+  for(date in arrayOfDates){
+      totalDays += 1
+      totalDJU += arrayOfDates[date].DJU
+  }
+
+  let moyenneDJU = totalDJU / totalDays
+  // console.log(moyenneDJU)
+  return moyenneDJU
+}
+
+const calculDJUDecennie = (startDate,endDate) => {
+
+let DJUMoyenDecennie = 0
+
+  for (let i=0;i<10;i++){
+    console.log('Hiver ' + startDate + '/'+ endDate, calculDJUMoyen(extractWinterOf(startDate,endDate)))
+    DJUMoyenDecennie += calculDJUMoyen(extractWinterOf(startDate,endDate))
+    startDate = parseInt(startDate)+1
+    endDate = parseInt(endDate)+1
+  }
+
+DJUMoyenDecennie = DJUMoyenDecennie / 10
+
+return DJUMoyenDecennie
+
+}
+
+
 
 
 
